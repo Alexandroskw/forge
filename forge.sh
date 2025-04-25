434572b0e2458fe7baf0f896779cd8f6ef1c8d54
Alexandroskw <filipbilly.120@gmail.com>
2025-04-25 12:52:18 -0600

    fix: Implementing the function for install recursively the packages

--- forge.sh
+++ forge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Forcing the script running as sudo
-if [ "$EUID" -ne 0]; then
+if [ "$EUID" -ne 0 ]; then
         echo "Run this script as sudo ($0)"
         echo "Exiting..."
         exit 1
@@ -15,13 +15,13 @@ echo "Updating the System. Please wait..."
 dnf upgrade -y
 dnf autoremove -y
 
-# Cleaning the cache of the DNF system
+# Cleaning the cache of the DNF package system
 dnf clean all
 
 # Necessary arrays for the instalation
-souce "$(dirname "$0")/pack.conf"
+source "$(dirname "$0")/pack.conf"
 
-# Verifying the existance of folders ".icons", ".themes", ".fonts" for the dotfiles
+# Verifying the existence of folders ".icons", ".themes", ".fonts" for the dotfiles
 for dir in "${DIRECTORIES[@]}"; do
         if [ ! -d "$dir" ]; then
                 echo "The directory $dir not existing. Creating..."
@@ -30,3 +30,25 @@ for dir in "${DIRECTORIES[@]}"; do
                 echo "The directory already exist. Skipping..."
         fi
 done
+
+# The array is "forged" or installed?
+is_forged(){
+        dnf -q "$1" &> /dev/null
+}
+
+# Function for install all the packages for the pack.conf
+forging_packages() {
+        local packages=("$@")
+        local forge=()
+
+        for f in "${packages[@]}"; do
+                if ! is_forged "$f"
+                        forge+=("$f")
+                fi
+
+                if [ ${#forge[@]} -ne 0 ]; then
+                        echo "Installing: ${forge[*]}}"
+                        dnf install -y "${forge[@]}"
+                fi
+        done
+}
