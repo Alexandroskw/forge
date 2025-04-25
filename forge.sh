1fc524c86c28c99c2ccb96face6be4a32a35b145
Alexandroskw <filipbilly.120@gmail.com>
2025-04-24 22:01:00 -0600

    add: Verifying the necessary directories for the dotfiles

--- forge.sh
+++ forge.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Forcing the script is running as sudo
+# Forcing the script running as sudo
 if [ "$EUID" -ne 0]; then
         echo "Run this script as sudo ($0)"
         echo "Exiting..."
@@ -12,9 +12,21 @@ clear
 
 # Updating Fedora
 echo "Updating the System. Please wait..."
-
 dnf upgrade -y
 dnf autoremove -y
 
 # Cleaning the cache of the DNF system
 dnf clean all
+
+# Necessary arrays for the instalation
+souce "$(dirname "$0")/pack.conf"
+
+# Verifying the existance of folders ".icons", ".themes", ".fonts" for the dotfiles
+for dir in "${DIRECTORIES[@]}"; do
+        if [ ! -d "$dir" ]; then
+                echo "The directory $dir not existing. Creating..."
+                mkdir -p "$dir"
+        else
+                echo "The directory already exist. Skipping..."
+        fi
+done
