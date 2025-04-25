6b1c0fd798c2becb4b71dbbdfda870ee62c94df1
Alexandroskw <filipbilly.120@gmail.com>
2025-04-24 21:26:41 -0600

    add: Forcing the user to run as **sudo** and updating the system

--- forge.sh
+++ forge.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
+# Forcing the script is running as sudo
+if [ "$EUID" -ne 0]; then
+        echo "Run this script as sudo ($0)"
+        echo "Exiting..."
+        exit 1
+fi
 
+# Cleaning the terminal
+clear
+
+# Updating Fedora
+echo "Updating the System. Please wait..."
+
+dnf upgrade -y
+dnf autoremove -y
+
+# Cleaning the cache of the DNF system
+dnf clean all
