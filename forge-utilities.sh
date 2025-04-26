497f26d02459add7fe0e52c611e0650c349f4da0
Alexandroskw <filipbilly.120@gmail.com>
2025-04-26 16:32:50 -0600

    add: Set the function for install the repos

--- forge-utilities.sh
+++ forge-utilities.sh
@@ -34,28 +34,39 @@ fi
 # Cloning the Alacritty themes repository
 cd "$HOME/.config/alacritty/" # Changing to the alacritty config path
 
-
-if [ ! -d "$THEMES_FOLDER" ]; then
-        echo "The folder is not forged. Creating and cloning..."
-        # Creating the folder for the Alacritty themes repo
-        git clone "$ALACRITTY_THEMES_URL" "$THEMES_FOLDER"
-else
-        echo "The folder is forged. Skipping..."
-fi
+cloning_repos "$DOTFILES_URL" "$NAME"
+# if [ ! -d "$THEMES_FOLDER" ]; then
+#         echo "The folder is not forged. Creating and cloning..."
+#         # Creating the folder for the Alacritty themes repo
+#         git clone "$ALACRITTY_THEMES_URL" "$THEMES_FOLDER"
+# else
+#         echo "The folder is forged. Skipping..."
+# fi
 
 # Cloning the TPM for TMUX
-if [ -d "$TPM_DIR" ]; then
-        echo "The folder is not forged. Creating and cloning..."
-fi
+# if [ -d "$TPM_DIR" ]; then
+#         echo "The folder is not forged. Creating and cloning..."
+# fi
 
 # Installing Starship framework
 STARSHIP="curl -sS https://starship.rs/install.sh | sh"
 
-# Function for clone all the packages
+# Function for clone all the repos
 cloning_repos() {
-        local repo=("$@")
+        local repo_url="$1"
+        local dir_target="$2"
 
-        if [ ! -d "$@" ]; then
-                echo "The ${@} is not forged. Creating and cloning"
+        if [ -d "$dir_target" ]; then
+                echo "The $dir_target already cloned. Skipping..."
+        else
+                echo "Forging the repo..."
+                git clone "$repo_url" "$dir_target"
+        fi
+
+        if [ $? -eq 0 ];then
+                echo "Forge of $dir_target was sucessful"
+        else
+                echo "Failed to clone the $dir_target"
+                exit 1
         fi
 }
