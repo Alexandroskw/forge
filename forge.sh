6ae813fddd720437a9f3e15ac0bb28504069aca5
Alexandroskw <filipbilly.120@gmail.com>
2025-04-30 14:00:40 -0600

    del: Deleting the `sudo bash -c` command. Using regular `sudo dnf`

--- forge.sh
+++ forge.sh
@@ -20,34 +20,34 @@ logo
 
 # Updating Fedora
 echo "Updating the system (your password is required). Please wait..."
-sudo bash -c "
-        sudo dnf upgrade -y
-        sudo dnf autoremove -y
+sudo dnf upgrade -y
+sudo dnf autoremove -y
 
-        # Cleaning the cache of the DNF package system
-        sudo dnf clean all
-        # The array is "forged" or installed?
-        is_forged(){
-                sudo dnf -q "$1" &> /dev/null
-        }
+# Cleaning the cache of the DNF package system
+sudo dnf clean all
 
-        # Function for install all the packages for the pack.conf arrays
-        forging_packages() {
-                local packages=("$@")
-                local to_forge=()
-        
-                for f in "${packages[@]}"; do
-                        if ! is_forged "$f"; then
-                                to_forge+=("$f")
-                        fi
-                done
-        
-                if [ ${#to_forge[@]} -ne 0 ]; then
-                        echo "Installing: ${to_forge[*]}"
-                        sudo dnf install -y "${to_forge[@]}"
+# The array is "forged" or installed?
+is_forged(){
+        sudo dnf -q "$1" &> /dev/null
+}
+
+# Function for install all the packages for the pack.conf arrays
+forging_packages() {
+        local packages=("$@")
+        local to_forge=()
+
+        for f in "${packages[@]}"; do
+                if ! is_forged "$f"; then
+                        to_forge+=("$f")
                 fi
-        }
-"
+        done
+
+        if [ ${#to_forge[@]} -ne 0 ]; then
+                echo "Installing: ${to_forge[*]}"
+                sudo dnf install -y "${to_forge[@]}"
+        fi
+}
+
 # Necessary arrays for the instalation
 source "$(dirname "$0")/pack.conf"
 
@@ -56,7 +56,6 @@ if [ ! -f "pack.conf" ]; then
         echo "The forge can't start. The 'pack.conf' not found"
         exit 1
 fi
-
 
 # Installing the packages
 echo "󰢛 Forging the system utilities"
@@ -70,7 +69,6 @@ forging_packages "${MEDIA_UTILS[@]}"
 
 echo "󰢛 Forging the desktop utilities"
 forging_packages "${DESK_UTILS[@]}"
-
 
 # Verifying the existence of folders ".icons", ".themes", ".fonts" for the dotfiles
 for dir in "${DIRECTORIES[@]}"; do
